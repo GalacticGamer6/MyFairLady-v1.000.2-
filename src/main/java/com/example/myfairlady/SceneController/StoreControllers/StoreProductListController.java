f27559af951b26a170d07cda7eea4f3db474c78f
GalacticGamer6 <neeraav64@gmail.com>
2023-09-19 00:01:25 +0200

    had to revert to fix?

--- src/main/java/com/example/myfairlady/SceneController/StoreControllers/StoreProductListController.java
+++ src/main/java/com/example/myfairlady/SceneController/StoreControllers/StoreProductListController.java
@@ -1,4 +1,31 @@
 package com.example.myfairlady.SceneController.StoreControllers;
 
-public class StoreProductListController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StoreProductListController implements Initializable {
+
+    @FXML
+    private Button products_list_button;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+
+    }
+
+    //need to display and populate the table
+    public void populateTable(){
+
+
+
+    }
+
+
 }
